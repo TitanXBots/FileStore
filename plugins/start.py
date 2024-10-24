eae7e66704e58a3b69d15c25e811f5d36b1ea246
Yash <titanmattersbot05@gmail.com>
2024-10-24 15:50:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -96,7 +96,8 @@ async def start_command(client: Client, message: Message):
                 id = message.from_user.id
             ),
             reply_markup = reply_markup,
-            
+            disable_web_page_preview = True,
+            quote = True
         )
         return
     
@@ -144,8 +145,10 @@ async def not_joined(client: Client, message: Message):
         mention=message.from_user.mention,
         id=message.from_user.id
     ),
-    reply_markup=InlineKeyboardMarkup(buttons)
-)
+    reply_markup = InlineKeyboardMarkup(buttons),
+        quote = True,
+        disable_web_page_preview = True
+    )
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
